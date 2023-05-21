a7562cc0f9aea3837825538c3ddd7e416bae6835
Adithyan <sadithyan403@gmail.com>
2023-05-21 11:21:20 +0530

    hbs module added

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var values=["adit","ajay","arjun s"]
+  res.render('index', {values});
 });
 
 module.exports = router;
